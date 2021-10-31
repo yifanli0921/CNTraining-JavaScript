436c741b91ca2a925cd84abae68c97baf24f29a7
yifan li <yifan.li@thoguhtworks.com>
2021-10-31 23:52:13 +0800

    [yifan li]feat:js homework.

--- chapter-one/homework/practise.js
+++ chapter-one/homework/practise.js
@@ -1,8 +1,10 @@
 //问题1
 const removeRepeat = () => {
-    const array = [1, 2, 2, 3, 3, 3, 'a', 'a', 'b'];
+    const arr = [1, 2, 2, 3, 3, 3, 'a', 'a', 'b'];
     // todo
     // 输出[1, 2, 3, "a", "b"]
+    const a = [...new Set(arr)]
+    console.log(a);
 }
 removeRepeat();
 
@@ -10,14 +12,18 @@ removeRepeat();
 const findRepeat= () => {
     const array = [1, 2, 2, 3, 3, 3, 'a', 'a', 'b'];
     // todo
+    const result = [...new Set(array.filter(i => array.indexOf(i) !== array.lastIndexOf(i)))]
+    console.log(result);
     // 输出[2, 3, "a"]
 }
 findRepeat();
 
 
 //问题3
-const clone= () => {
+const clone= (obj) => {
+
     //todo
+    return{...obj}
 }
 const obj = {
     arr: [{a: 1, f: {q: 1}}, 2, 3],
@@ -31,9 +37,11 @@ console.log(obj1.string === obj.string);
 //问题4
 const question4= () => {
     for (var i = 0; i < 5; i++) {
-        setTimeout(function() {
-            console.log(i);
-        }, 1000);
+        (j => {
+            setTimeout(function() {
+                console.log(j)
+            }, j * 1000)
+        })(i)
     }
     console.log(i)
     // todo
